8a09a6daaf0613b41926e352e32d0339c32f5174
Greg Finzer <gfinzer@hotmail.com>
2023-01-28 10:19:06 -0500

    Copyright changed to 2023

--- SerializationConsoleTest/Properties/AssemblyInfo.cs
+++ SerializationConsoleTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kellerman Software")]
 [assembly: AssemblyProduct("SerializationConsoleTest")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
